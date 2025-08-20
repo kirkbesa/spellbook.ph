d46dd3312e6636ae6561cfe4e7435eae739361d4
Kirk Besa <kirkpatrickbesa@gmail.com>
2025-08-20 11:50:33 +0800

    fix: auth mt

--- src/pages/auth/AuthLayout.tsx
+++ src/pages/auth/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from '@tanstack/react-router'
 
 const AuthLayout = () => {
     return (
-        <div className='my-auto flex justify-center items-center'>
+        <div className='my-auto flex justify-center items-center mt-32'>
             <Outlet />
         </div>
     )
